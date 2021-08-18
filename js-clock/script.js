985898ae934384c92fb3a20ea8ee4f3851da64de
Tianika <tatyriana@yandex.ru>
2021-08-18 23:52:18 +0300

    feat: add background images and random change first bg

--- js-clock/script.js
+++ js-clock/script.js
@@ -4,26 +4,31 @@ const BACKGROUNDS = [
   'carla-cervantes-0FxXZWxaWt0-unsplash',
   'frank-mckenna-4V8JxijgZ_c-unsplash',
   'jake-blucker-bYJz2UAIAGI-unsplash',
-  'johannes-plenio-kfxBc2NQ18-unsplash',
   'media-ueBIGLmiI5A-unsplash',
   'nelson-santos-jr-PuaIPFH3FHg-unsplash',
   'oc-gonzalez-xg8z_KhSorQ-unsplash',
-  'plenio-qkfxBc2NQ18-unsplash',
-  'zoltan-tasi-8Ce-NRKCq4s-unsplash',
+  'simon-hajducki-QLL9CGveAO0-unsplash',
+  'aleksandr-ledogorov-G-JJy-Yv_dA-unsplash',
+  'mike-aunzo-yYSnv8meSMY-unsplash',
+  'dave-hoefler-D-FI-GHZeVc-unsplash',
+  'akk-U3wLZLmsVG8-unsplash',
+  'eric-scherrer-Xuv4SC-ld4M-unsplash',
+  'anders-jilden-uwbajDCODj4-unsplash',
+  'simon-marsault-5oow5G6BCGY-unsplash',
+  'anton-darius-xYIuqpHD2oQ-unsplash',
 ]
 
 const leftBtn = document.querySelector('.arrow-left')
 const rightBtn = document.querySelector('.arrow-right')
 
-let bgIndex = 6
+let bgIndex = Math.round(Math.random() * BACKGROUNDS.length)
+changeBackground(bgIndex)
 
 leftBtn.addEventListener('click', () => {
-  console.log('left')
   change('left')
 })
 
 rightBtn.addEventListener('click', () => {
-  console.log('right')
   change('right')
 })
 
