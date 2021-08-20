db1aa8afc28fee4a36a086fc7f6289f5f4e7d964
Tianika <tatyriana@yandex.ru>
2021-08-20 20:02:37 +0300

    feat: add digital clock

--- js-clock/script.js
+++ js-clock/script.js
@@ -1,14 +1,8 @@
 const BACKGROUNDS = [
   'byron-johnson-ec2SZSGPwJA-unsplash',
   'de-jesus-benitez-kEgJVDkQkbU-unsplash',
-  'carla-cervantes-0FxXZWxaWt0-unsplash',
   'frank-mckenna-4V8JxijgZ_c-unsplash',
-  'jake-blucker-bYJz2UAIAGI-unsplash',
-  'media-ueBIGLmiI5A-unsplash',
   'nelson-santos-jr-PuaIPFH3FHg-unsplash',
-  'oc-gonzalez-xg8z_KhSorQ-unsplash',
-  'simon-hajducki-QLL9CGveAO0-unsplash',
-  'aleksandr-ledogorov-G-JJy-Yv_dA-unsplash',
   'mike-aunzo-yYSnv8meSMY-unsplash',
   'dave-hoefler-D-FI-GHZeVc-unsplash',
   'akk-U3wLZLmsVG8-unsplash',
@@ -17,20 +11,68 @@ const BACKGROUNDS = [
   'simon-marsault-5oow5G6BCGY-unsplash',
   'anton-darius-xYIuqpHD2oQ-unsplash',
 ]
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+const DAYS = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 
 const leftBtn = document.querySelector('.arrow-left')
 const rightBtn = document.querySelector('.arrow-right')
+const time = document.querySelector('.digital-time')
+const dateYear = document.querySelector('.digital-date')
 
 let bgIndex = Math.round(Math.random() * BACKGROUNDS.length)
 changeBackground(bgIndex)
 
-leftBtn.addEventListener('click', () => {
-  change('left')
-})
+let timerId = setInterval(() => showTime(), 1000)
 
-rightBtn.addEventListener('click', () => {
-  change('right')
-})
+leftBtn.addEventListener('click', () => change('left'))
+rightBtn.addEventListener('click', () => change('right'))
+
+showTime()
+
+function showTime() {
+  let now = new Date()
+  let year = now.getFullYear()
+  let month = now.getMonth()
+  let date = now.getDate()
+  let day = now.getDay()
+  let hours = now.getHours()
+  let minutes = now.getMinutes()
+  let seconds = now.getSeconds()
+
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`
+  }
+
+  time.textContent = `${hours} : ${minutes} : ${seconds}`
+  dateYear.textContent = `${DAYS[day]}, ${date} ${MONTHS[month]} ${year}`
+}
 
 function change(direction) {
   if (direction === 'left') {
@@ -53,3 +95,5 @@ function change(direction) {
 function changeBackground(bgIndex) {
   document.body.style.backgroundImage = `url(/img/${BACKGROUNDS[bgIndex]}.jpg)`
 }
+
+timerId()
