a6cf08b17215375a56f2fc5cd68db27ca6b368a4
Tianika <tatyriana@yandex.ru>
2021-08-21 16:19:43 +0300

    feat: add audio

--- js-clock/script.js
+++ js-clock/script.js
@@ -1,16 +1,35 @@
 const BACKGROUNDS = [
   'byron-johnson-ec2SZSGPwJA-unsplash',
   'de-jesus-benitez-kEgJVDkQkbU-unsplash',
+  'irina-iriser-t1hVc9bqai0-unsplash',
   'frank-mckenna-4V8JxijgZ_c-unsplash',
+  'irina-iriser-uPKgTRAflLY-unsplash',
   'nelson-santos-jr-PuaIPFH3FHg-unsplash',
   'mike-aunzo-yYSnv8meSMY-unsplash',
+  'karl-jk-hedin-8Cj3qmU2QtI-unsplash',
   'dave-hoefler-D-FI-GHZeVc-unsplash',
+  'luca-cavallin-PYlO9l71i3w-unsplash',
   'akk-U3wLZLmsVG8-unsplash',
   'eric-scherrer-Xuv4SC-ld4M-unsplash',
+  'johannes-plenio-s0XDLfhyN34-unsplash',
   'anders-jilden-uwbajDCODj4-unsplash',
   'simon-marsault-5oow5G6BCGY-unsplash',
+  'freestocks-EssPg6x5QeY-unsplash',
   'anton-darius-xYIuqpHD2oQ-unsplash',
 ]
+
+const SOUNDS = [
+  'les-ptitsyi-techet-reka-36679',
+  'les-yujnaya-amerika-vecher-nasekomyie-blizko-38554',
+  'more-slegka-volnuetsya',
+  'priroda-noch-sverchki-36291',
+  'priroda-noch-sverchki-techet-reka-37383',
+  'reka-malenkaya-31368',
+  'reka-techt-omyivaya-kamni-medlennoe-techenie-31243',
+  'spokoynoe-more-i-kriki-chaek',
+  'volnyi-pleschutsya-more-okean-pobereje-41080',
+]
+
 const MONTHS = [
   'January',
   'February',
@@ -39,37 +58,53 @@ const leftBtn = document.querySelector('.arrow-left')
 const rightBtn = document.querySelector('.arrow-right')
 const timeStr = document.querySelector('.digital-time')
 const dateStr = document.querySelector('.digital-date')
+const hourHands = document.querySelector('.hours')
+const minutesHands = document.querySelector('.minutes')
+const secondHands = document.querySelector('.seconds')
+const audio = document.querySelector('.audio')
+const leftSound = document.querySelector('.sound-left')
+const rightSound = document.querySelector('.sound-right')
 
 let bgIndex = Math.round(Math.random() * BACKGROUNDS.length)
 
 let timerId = setTimeout(function show() {
-  getTime()
+  setTime()
+  analogClock()
   setTimeout(show, 1000)
 }, 1000 - new Date().getMilliseconds())
 
-leftBtn.addEventListener('click', () => change('left'))
-rightBtn.addEventListener('click', () => change('right'))
+leftBtn.addEventListener('click', () => change('left', BACKGROUNDS))
+rightBtn.addEventListener('click', () => change('right', BACKGROUNDS))
+leftSound.addEventListener('click', () => change('left', SOUNDS))
+rightSound.addEventListener('click', () => change('right', SOUNDS))
 
-getTime()
 changeBackground(bgIndex)
+changeAudio(bgIndex)
+setTime()
 
-function analogTime() {
+function setIndex(arr) {
+  let bgIndex = Math.round(Math.random() * arr.length)
+}
+
+function analogClock() {
+  let now = new Date()
+
   let hours = now.getHours()
   let minutes = now.getMinutes()
   let seconds = now.getSeconds()
 
-  if (hours < 10) {
-    hours = `0${hours}`
-  }
-  if (minutes < 10) {
-    minutes = `0${minutes}`
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`
-  }
+  let hourDeg = hours * 30
+  let minutesDeg = minutes * 6
+  let secondsDeg = seconds * 6
+
+  hourHands.style.transform = `rotate(${hourDeg}deg)`
+  minutesHands.style.transform = `rotate(${minutesDeg}deg)`
+  secondHands.style.transform = `rotate(${secondsDeg}deg)`
 }
 
-function getTime() {
+function setTimeClock() {}
+
+function setTime() {
   let now = new Date()
   let options = {
     weekday: 'long',
@@ -88,16 +123,16 @@ function showTime(time, date) {
   dateStr.textContent = `${date}`
 }
 
-function change(direction) {
+function change(direction, arr) {
   if (direction === 'left') {
     if (bgIndex > 0) {
       bgIndex--
     } else {
-      bgIndex = BACKGROUNDS.length - 1
+      bgIndex = arr.length - 1
     }
   }
   if (direction === 'right') {
-    if (bgIndex < BACKGROUNDS.length - 1) {
+    if (bgIndex < arr.length - 1) {
       bgIndex++
     } else {
       bgIndex = 0
@@ -109,4 +144,8 @@ function change(direction) {
 
 function changeBackground(bgIndex) {
   document.body.style.backgroundImage = `url(/img/${BACKGROUNDS[bgIndex]}.jpg)`
+}
+
+function changeAudio(bgIndex) {
+  audio.style.src = `/mp3/${SOUNDS[bgIndex]}.mp3`
 }
