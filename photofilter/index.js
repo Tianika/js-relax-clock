06c94a9166e368b46742d2e836ea085ccaa0f303
Tianika <tatyriana@yandex.ru>
2021-08-29 20:58:10 +0300

    feat: add gallery images

--- photofilter/index.js
+++ photofilter/index.js
@@ -1,4 +1,12 @@
+const presets = {
+  default: `blur(0px) brightness(1) contrast(1) grayscale(0) invert(0)
+  saturate(1) hue-rotate(0deg) sepia(0)`,
+}
+
 const inputs = document.querySelectorAll('.controls input')
+const reset = document.querySelector('.reset')
+const photo = document.querySelector('.photo-main')
+const galleryBtns = document.querySelectorAll('.gallery-img')
 
 function filterUpdate() {
   const suff = this.dataset.sizing || ''
@@ -29,5 +37,17 @@ function changeColor(vari, color) {
   document.documentElement.style.setProperty(vari, color)
 }
 
+function resetInputs() {
+  const reset = document.querySelector('.photo-main')
+  reset.style.filter = presets['default']
+}
+
+function changePhoto() {
+  photo.src = this.src
+}
+
 inputs.forEach((input) => input.addEventListener('change', filterUpdate))
 inputs.forEach((input) => input.addEventListener('mousemove', filterUpdate))
+galleryBtns.forEach((button) => button.addEventListener('click', changePhoto))
+
+//reset.addEventListener('click', resetInputs)
