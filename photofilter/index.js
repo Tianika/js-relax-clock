696437c8c998ec59cb92bfc9313fe76e33eafd11
Tianika <tatyriana@yandex.ru>
2021-08-29 22:28:27 +0300

    feat: add presets

--- photofilter/index.js
+++ photofilter/index.js
@@ -1,24 +1,30 @@
-const presets = {
-  default: `blur(0px) brightness(1) contrast(1) grayscale(0) invert(0)
-  saturate(1) hue-rotate(0deg) sepia(0)`,
+const DEFAULTVALUES = {
+  blur: '0px',
+  brightness: '1',
+  contrast: '1',
+  grayscale: '0',
+  invert: '0',
+  saturate: '1',
+  'hue-rotate': '0deg',
+  sepia: '0',
 }
 
+const root = document.querySelector(':root')
 const inputs = document.querySelectorAll('.controls input')
 const reset = document.querySelector('.reset')
 const photo = document.querySelector('.photo-main')
+const presets = document.querySelectorAll('.presets-img')
 const galleryBtns = document.querySelectorAll('.gallery-img')
+const load = document.querySelector('.load')
+const save = document.querySelector('.save')
 
 function filterUpdate() {
   const suff = this.dataset.sizing || ''
-  document.documentElement.style.setProperty(
-    `--${this.name}`,
-    this.value + suff
-  )
+  root.style.setProperty(`--${this.name}`, this.value + suff)
   checkBgColor()
 }
 
 function checkBgColor() {
-  const root = document.querySelector(':root')
   const color = getComputedStyle(root).getPropertyValue('--background')
   const currColor = Number(`0x${color.substring(1, 7)}`)
 
@@ -38,16 +44,33 @@ function changeColor(vari, color) {
 }
 
 function resetInputs() {
-  const reset = document.querySelector('.photo-main')
-  reset.style.filter = presets['default']
+  photo.className = 'photo-main'
 }
 
 function changePhoto() {
-  photo.src = this.src
+  let imgUrl = this.src
+  photo.src = imgUrl
+  presets.forEach((preset) => {
+    preset.src = imgUrl
+  })
 }
+
+function setPreset() {
+  let style = this.dataset.name
+  resetInputs()
+  photo.classList.add(style)
+}
+
+function saveFile() {}
+
+function loadFile() {}
 
 inputs.forEach((input) => input.addEventListener('change', filterUpdate))
 inputs.forEach((input) => input.addEventListener('mousemove', filterUpdate))
 galleryBtns.forEach((button) => button.addEventListener('click', changePhoto))
+presets.forEach((img) => img.addEventListener('click', setPreset))
+reset.addEventListener('click', resetInputs)
+save.addEventListener('click', saveFile)
+load.addEventListener('click', loadFile)
 
-//reset.addEventListener('click', resetInputs)
+//root.style.setProperty('--main-color', '#88d8b0');
